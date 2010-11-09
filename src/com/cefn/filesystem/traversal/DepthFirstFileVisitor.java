afcc426fafb794f31dd9f00a7914360b5110a753
Cefn Hoile <allyourx.github@cefn.com>
2010-11-09 18:14:00 +0000

    Traversal classes

---
+++ src/com/cefn/filesystem/traversal/DepthFirstFileVisitor.java
@@ -0,0 +1,36 @@
+package com.cefn.filesystem.traversal;
+
+import com.cefn.filesystem.File;
+import com.cefn.filesystem.Filesystem;
+import com.cefn.filesystem.Folder;
+import com.cefn.filesystem.Visitor;
+
+public abstract class DepthFirstFileVisitor{
+
+	VisitableFactory visitableFactory;
+	
+	public DepthFirstFileVisitor(VisitableFactory visitableFactory) {
+		this.visitableFactory = visitableFactory;
+	}
+		
+	public void visit(Filesystem f){
+		visitableFactory.getFolderVisitable(f).accept(new Visitor<Folder>() {
+			@Override
+			public void visit(Folder item) {
+				DepthFirstFileVisitor.this.visit(item);
+			}
+		});
+	}
+
+	public void visit(Folder f){
+		visitableFactory.getFileVisitable(f).accept(new Visitor<File>() {
+			@Override
+			public void visit(File item) {
+				DepthFirstFileVisitor.this.visit(item);
+			}
+		});						
+	}
+	
+	public abstract void visit(File f);	
+	
+}
---
+++ src/com/cefn/filesystem/traversal/StoredVisitableFactory.java
@@ -0,0 +1,54 @@
+package com.cefn.filesystem.traversal;
+
+import java.util.Iterator;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import com.cefn.filesystem.File;
+import com.cefn.filesystem.Filesystem;
+import com.cefn.filesystem.Folder;
+import com.cefn.filesystem.Visitable;
+import com.cefn.filesystem.Visitor;
+
+public class StoredVisitableFactory implements VisitableFactory{
+	
+	private EntityManager entityManager;
+	
+	public StoredVisitableFactory(EntityManager entityManager){
+		this.entityManager = entityManager;
+	}
+
+	@Override
+	public Visitable<Folder> getFolderVisitable(final Filesystem fs) {
+		return new Visitable<Folder>(){
+			@Override
+			public void accept(Visitor<Folder> visitor) {
+				Query query = entityManager.createQuery("SELECT folder FROM Folder folder WHERE folder.filesystem = :filesystem");
+				query.setParameter("filesystem", fs);
+				Iterator<Folder> folderIterator = ((List<Folder>)query.getResultList()).iterator();
+				while(folderIterator.hasNext()){
+					visitor.visit(folderIterator.next());
+				}
+			}
+		};
+	}
+
+	@Override
+	public Visitable<File> getFileVisitable(final Folder fs) {
+		return new Visitable<File>(){
+			@Override
+			public void accept(Visitor<File> visitor) {
+				Query query = entityManager.createQuery("SELECT folder FROM File folder WHERE folder.filesystem = :filesystem");
+				query.setParameter("folder", fs);
+				Iterator<File> fileIterator = ((List<File>)query.getResultList()).iterator();
+				while(fileIterator.hasNext()){
+					visitor.visit(fileIterator.next());
+				}
+			}
+		};
+	}
+
+	
+}
